a41c3090b7e5a86f5c3a094504a00033b1771ef1
andreMarcosPerez <andre.marcos.perez@gmail.com>
2019-11-17 11:34:45 -0300

    starchema publicacoes 1.b question resolution

--- database/star-schema-publicacoes/query-db.sql
+++ database/star-schema-publicacoes/query-db.sql
@@ -18,6 +18,25 @@ USE publicacao;
 
 # - Question 1.b
 
-select sum(fact.quantidade_publicacao) as total_publicacoes
-from fact_publicacao fact join dim_data data on fact.data_id = data.id 
-where data.mes = 11 and data.ano = 2019;
+select sum(fact.quantidade_publicacao) as total_pub
+from fact_publicacao fact 
+join dim_data data 
+on fact.data_id = data.id 
+where 1=1
+and data.mes = 11 
+and data.ano = 2019;
+
+select sum(fact.quantidade_publicacao) as total_pub
+from fact_publicacao fact 
+join dim_autor autor 
+on fact.autor_id = autor.id 
+where 1=1 
+and autor.departamento_sigla = 'SCC' 
+and autor.instituto_sigla = 'ICMC' 
+and autor.universidade_sigla = 'USP';
+
+select sum(fact.quantidade_publicacao) as total_pub
+from fact_publicacao fact 
+join dim_local_publicacao local_publicacao 
+on fact.local_publicacao_id = local_publicacao.id 
+where local_publicacao.classificacao_qualis = 'A2';
